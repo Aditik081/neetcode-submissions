5a73fb9c3d5cf2f21ef3093500ac8530bea46e82
Aditik081 <aditik605@gmail.com>
2026-08-05 02:13:47 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+
+        int mini=prices[0];        
+        int res=0;
+
+        for(int i=0;i<prices.size();i++){
+            mini=min(mini,prices[i]);
+            res=max(res,prices[i]-mini);
+        }
+        return res;
+            
+    }
+};
